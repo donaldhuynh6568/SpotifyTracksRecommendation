b4b413bfdf1408b54e2cc478cc7245c5aa29eb98
donaldhuynh6568 <53349269+donaldhuynh6568@users.noreply.github.com>
2021-11-07 19:50:50 -0800

    Update Secret.py

--- Secret.py
+++ Secret.py
@@ -4,10 +4,10 @@ Created on Tue Sep 28 21:28:15 2021
 
 @author: kimje
 """
+#Retrieve this information from Spotify Developer
+clientID= "your clientID here"
 
-clientID= "dcc21a37cc5043128d21d9f13ae66490"
-
-clientSecret = "90cbe933faf6455481044dfc6e401af0"
+clientSecret = "your secret here"
 
 redirectUri= "https%3A%2F%2Fgoogle.com%2F"
 
