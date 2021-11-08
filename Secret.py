971f1b02fef2a2cb0a080e1ad835186285b2717a
donaldhuynh6568 <53349269+donaldhuynh6568@users.noreply.github.com>
2021-11-07 19:49:58 -0800

    Add files via upload

---
+++ Secret.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 28 21:28:15 2021
+
+@author: kimje
+"""
+
+clientID= "dcc21a37cc5043128d21d9f13ae66490"
+
+clientSecret = "90cbe933faf6455481044dfc6e401af0"
+
+redirectUri= "https%3A%2F%2Fgoogle.com%2F"
+
+userID='c0wmilker247'
